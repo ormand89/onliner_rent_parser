892adbeea86b0d5facb02251784bdf7e5d01cef8
Ormand89 <komoskoav@gmail.com>
2017-05-23 18:56:47 +0300

    Add console_controller_spec

--- spec/lib/console_controller_spec.rb
+++ spec/lib/console_controller_spec.rb
@@ -3,27 +3,55 @@ require 'console_controller'
 
 RSpec.describe OptionsParser do
   describe '#parse_options' do
-    shared_examples 'check parse order' do |args:, rent_type:, price_min:, price_max:, metro:, only_owner:, sort:, format:|
+    shared_examples 'check parse order' do |args:, rent:, price_min:, price_max:, underground:, owner:, sort:, format:|
       context "for options #{args}" do
         before { ARGV.push(*args) }
         subject(:console) { described_class.new }
-        let(:options) { [tag, size, picture_number] }
+        let(:flat_hash) {
+          { rent_type: rent,
+            price: { min: price_min, max: price_max },
+            currency: 'usd',
+            metro: underground,
+            only_owner: owner,
+            bounds: { lb: { lat: 53.77865438306248, long: 27.368307803348014 },
+                      rt: { lat: 54.02541191840544, long: 27.75637209747086 } },
+            page: 1 } }
+        let(:options) { [format, sort, flat_hash] }
 
-        it 'return tag' do
+        it 'return correct options' do
           expect(console.parse_options ).to eql(options)
         end
       end
     end
 
-    include_examples 'check parse order', args: ['--rent_type', 'pony', '--price_min', 'big'],
-                                          tag: 'Pony', size: 'Big', picture_number: 10
-    include_examples 'check parse order', args: ['--tag', 'tank', '--size', 'large'],
-                                          tag: 'Tank', size: 'Large', picture_number: 10
-    include_examples 'check parse order', args: ['--tag', 'Cats'],
-                                          tag: 'Cats', size: 'Small', picture_number: 10
-    include_examples 'check parse order', args: ['--tag', 'Flowers', '--size', 'big', '--quanity', '20'],
-                                          tag: 'Flowers', size: 'Big', picture_number: 20
-    include_examples 'check parse order', args: [],
-                                          tag: 'Kitties', size: 'Small', picture_number: 10
+    include_examples 'check parse order',
+    args: ['--rent_type', 'room,1_room', '--price_min', '100', '--price_max', '500'],
+    rent: ['room', '1_room'],
+    price_min: '100',
+    price_max: '500',
+    format: 'json',
+    sort: false,
+    underground: ['red_line', 'blue_line'],
+    owner: false
+
+    include_examples 'check parse order',
+    args: ['--rent_type', '2_room', '--price_min', '50', '--price_max', '400', '--file_format', 'csv'],
+    rent: ['2_room'],
+    price_min: '50',
+    price_max: '400',
+    format: 'csv',
+    sort: false,
+    underground: ['red_line', 'blue_line'],
+    owner: false
+
+    include_examples 'check parse order',
+    args: ['--price_min', '300', '--price_max', '700', '--sort', 'true', '--metro', 'red_line' ],
+    rent: ['room', '1_room', '2_rooms', '3_rooms', '4_rooms', '5_rooms', '6_rooms'],
+    price_min: '300',
+    price_max: '700',
+    format: 'json',
+    sort: 'true',
+    underground: ['red_line'],
+    owner: false
   end
 end
