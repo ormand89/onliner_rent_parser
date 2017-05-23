a381dd0d9aaab7f5ed94bd503e4fd70c9f9238eb
Ormand89 <komoskoav@gmail.com>
2017-05-23 17:03:30 +0300

    Add csv_format

--- lib/format_services/csv_format.rb
+++ lib/format_services/csv_format.rb
@@ -1,5 +1,10 @@
+require 'csv'
+
 class CSVFormat < FormatService
 
   def write(apartments)
+    CSV.open('myfile.csv', 'w') do |f|
+      apartments.each { |apartment| apartment.description.to_a.each { |element| f << element } }
+    end
   end
 end
