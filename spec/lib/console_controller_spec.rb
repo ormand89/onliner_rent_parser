9b87bbe523083539bbcaf0f280df67e82c9b48b1
Ormand89 <komoskoav@gmail.com>
2017-05-24 18:38:06 +0300

    Correct console_controller_spec

--- spec/lib/console_controller_spec.rb
+++ spec/lib/console_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 require 'console_controller'
 
+OUTPUT_PATH = File.expand_path("spec/fixtures", File.dirname(__FILE__))
+
 RSpec.describe OptionsParser do
   describe '#parse_options' do
     shared_examples 'check parse order' do |args:, rent:, price_min:, price_max:, underground:, owner:, sort:, format:|
@@ -15,8 +17,9 @@ RSpec.describe OptionsParser do
             bounds: { lb: { lat: 53.77865438306248, long: 27.368307803348014 },
                       rt: { lat: 54.02541191840544, long: 27.75637209747086 } },
             page: 1 } }
-        let(:options) { Options.new(format, sort, flat_hash) }
+        let(:options) { Options.new(format, sort, OUTPUT_PATH, flat_hash) }
         let(:parsed_options) { console.parse_options }
+
         it 'return correct options' do
           expect(parsed_options.apartments_parameters ).to eq(options.apartments_parameters)
           expect(parsed_options.sort_option ).to eq(options.sort_option)
@@ -26,33 +29,33 @@ RSpec.describe OptionsParser do
     end
 
     include_examples 'check parse order',
-    args: ['--rent_type', 'room,1_room', '--price_min', '100', '--price_max', '500'],
-    rent: ['room', '1_room'],
-    price_min: '100',
-    price_max: '500',
-    format: 'json',
-    sort: false,
-    underground: ['red_line', 'blue_line'],
-    owner: false
+      args: ['--rent_type', 'room,1_room', '--price_min', '100', '--price_max', '500'],
+      rent: ['room', '1_room'],
+      price_min: '100',
+      price_max: '500',
+      format: 'json',
+      sort: false,
+      underground: ['red_line', 'blue_line'],
+      owner: false
 
     include_examples 'check parse order',
-    args: ['--rent_type', '2_room', '--price_min', '50', '--price_max', '400', '--file_format', 'csv'],
-    rent: ['2_room'],
-    price_min: '50',
-    price_max: '400',
-    format: 'csv',
-    sort: false,
-    underground: ['red_line', 'blue_line'],
-    owner: false
+      args: ['--rent_type', '2_room', '--price_min', '50', '--price_max', '400', '--file_format', 'csv'],
+      rent: ['2_room'],
+      price_min: '50',
+      price_max: '400',
+      format: 'csv',
+      sort: false,
+      underground: ['red_line', 'blue_line'],
+      owner: false
 
     include_examples 'check parse order',
-    args: ['--price_min', '300', '--price_max', '700', '--sort', 'true', '--metro', 'red_line' ],
-    rent: ['room', '1_room', '2_rooms', '3_rooms', '4_rooms', '5_rooms', '6_rooms'],
-    price_min: '300',
-    price_max: '700',
-    format: 'json',
-    sort: 'true',
-    underground: ['red_line'],
-    owner: false
+      args: ['--price_min', '300', '--price_max', '700', '--sort', 'true', '--metro', 'red_line' ],
+      rent: ['room', '1_room', '2_rooms', '3_rooms', '4_rooms', '5_rooms', '6_rooms'],
+      price_min: '300',
+      price_max: '700',
+      format: 'json',
+      sort: 'true',
+      underground: ['red_line'],
+      owner: false
   end
 end
--- spec/lib/url_maker_spec.rb
+++ spec/lib/url_maker_spec.rb
@@ -39,7 +39,7 @@ RSpec.describe URLMaker do
 
   describe '#last_page' do
     it 'return correct page number' do
-      expect(url_maker.last_page(url)).to eql(3)
+      expect(url_maker.last_page(url)).to eql(2)
     end
   end
 
